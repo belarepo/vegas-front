84c1d0fa6e425211d351c3c3a3fbe458281b9531
Mahdi Asgari <mahdi1001101@gmail.com>
2022-05-27 00:21:06 +0430

    add responsive header

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,30 @@
 $(function () {
-	$('.loading-area').fadeOut()
+
+	// After loading mechanisms
+	$('.loading-area').fadeOut(1000);
+
+
+	// Menubar scroll effects
+	const hasToggled = function () {
+		let visible = false;
+		return function (scroll) {
+
+			if ((scroll > 140 && !visible) || (scroll < 140 && visible)) {
+				visible = !visible;
+				return true
+			}
+
+			return false
+		}
+	}();
+
+	$(document).scroll(function () {
+		if (hasToggled($(document).scrollTop())) {
+			
+			$('header').toggleClass('scrolldown');
+			$('.menu').toggleClass('scrolldown');
+		}
+	});
+
 });
 
