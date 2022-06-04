0b3de5cc5b024832c10a99a2c4cfcf7d57fc104a
Mahdi Asgari <mahdi1001101@gmail.com>
2022-06-05 01:27:35 +0430

    banner done

--- js/custom.js
+++ js/custom.js
@@ -1,49 +1,31 @@
 $(function () {
+  // After loading mechanisms
+  $('.loading-area').fadeOut(1000);
 
-	// After loading mechanisms
-	$('.loading-area').fadeOut(1000);
+  // Menubar scroll effects
+  const hasToggled = (function () {
+    let visible = false;
+    return function (scroll) {
+      if ((scroll > 50 && !visible) || (scroll < 50 && visible)) {
+        visible = !visible;
+        return true;
+      }
 
+      return false;
+    };
+  })();
 
-	// Menubar scroll effects
-	const hasToggled = function () {
-		let visible = false;
-		return function (scroll) {
+  $(document).scroll(function () {
+    if (hasToggled($(document).scrollTop())) {
+      $('header').toggleClass('scrolldown');
+      $('.menu').toggleClass('scrolldown');
+      $('.scrolltop').toggleClass('scrolldown');
+    }
+  });
 
-			if ((scroll > 140 && !visible) || (scroll < 140 && visible)) {
-				visible = !visible;
-				return true
-			}
-
-			return false
-		}
-	}();
-
-	$(document).scroll(function () {
-		if (hasToggled($(document).scrollTop())) {
-			
-			$('header').toggleClass('scrolldown');
-			$('.menu').toggleClass('scrolldown');
-			$('.scrolltop').toggleClass('scrolldown');
-		}
-	});
-
-	$('.samples-section-carousel').animate({
+  $('.samples-section-carousel').animate({
     left: '-' + $('#samples-section-item').css('width').slice(0, -2) * 4,
   });
 
-
-console.log()
-
-
-
-
-
-
-
-
-
-
-
-
+  console.log();
 });
-
